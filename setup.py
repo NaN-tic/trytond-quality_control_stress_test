0ee8f4f96d4032351de0d4f3bdf5a98d9d0b3826
Raimon Esteve <raimonesteve@gmail.com>
2025-05-20 12:55:46 +0200

    FIX Removing deprecated args from setup

--- setup.py
+++ setup.py
@@ -9,9 +9,7 @@ from configparser import ConfigParser
 
 MODULE = 'quality_control_stress_test'
 PREFIX = 'nantic'
-MODULE2PREFIX = {
-    'quality_control': 'nantic',
-    }
+MODULE2PREFIX = {}
 
 
 def read(fname):
@@ -58,14 +56,7 @@ if minor_version % 2:
 else:
     branch = series
 
-dependency_links = [
-    ('hg+https://bitbucket.org/nantic/'
-        'trytond-quality_control@%(branch)s'
-        '#egg=nantic-quality_control-%(series)s' % {
-            'branch': branch,
-            'series': series,
-            }),
-    ]
+dependency_links = []
 
 if minor_version % 2:
     # Add development index for testing with proteus
@@ -86,7 +77,7 @@ setup(name='%s_%s' % (PREFIX, MODULE),
         ],
     package_data={
         'trytond.modules.%s' % MODULE: (info.get('xml', [])
-            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', 'tests/*.rst']),
+            + ['tryton.cfg', 'locale/*.po', 'tests/*.rst']),
         },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
