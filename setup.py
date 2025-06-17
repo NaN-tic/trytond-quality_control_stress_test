6bb50893f11382c5ce013dc6bf01cc64e1b37db3
Raimon Esteve <raimonesteve@gmail.com>
2025-06-17 12:38:13 +0200

    FIX Removing deprecated args from setup

--- setup.py
+++ setup.py
@@ -9,7 +9,9 @@ from configparser import ConfigParser
 
 MODULE = 'quality_control_stress_test'
 PREFIX = 'nantic'
-MODULE2PREFIX = {}
+MODULE2PREFIX = {
+    'quality_control',
+}
 
 
 def read(fname):
@@ -64,12 +66,12 @@ if minor_version % 2:
 
 setup(name='%s_%s' % (PREFIX, MODULE),
     version=version,
-    description='',
+    description='%s' % MODULE,
     long_description=read('README'),
     author='NaN·tic',
     author_email='info@nan-tic.com',
     url='http://www.nan-tic.com/',
-    download_url="https://bitbucket.org/nantic/trytond-%s" % MODULE,
+    download_url="https://github.com/Nan-tic/trytond-%s" % MODULE,
     package_dir={'trytond.modules.%s' % MODULE: '.'},
     packages=[
         'trytond.modules.%s' % MODULE,
